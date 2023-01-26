18b9fcd59e548b0d012a9dffe0a41ce8bee88825
sasoria <sergio.alberto.arevalo.soria@nav.no>
2023-01-26 11:54:51 +0100

    Bedre navn på react komponenten

--- astro/mikrofrontends/shell/src/components/ReactComponent.jsx
+++ astro/mikrofrontends/shell/src/components/ReactComponent.jsx
@@ -2,11 +2,11 @@ import React from "https://www.nav.no/tms-min-side-assets/react/18/esm/index.js"
 import MicroFrontendA from "http://localhost:7100/bundle.js"
 import MicroFrontendB from "http://localhost:7200/bundle.js"
 
-const MyReactComponent = () => (
+const ReactComponent = () => (
   <>
     <MicroFrontendA />
     <MicroFrontendB />
   </>
 );
 
-export default MyReactComponent;
+export default ReactComponent;

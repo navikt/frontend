045803cf488ed22341c0a1d18b29221c9aae997f
sasoria <sergio.alberto.arevalo.soria@nav.no>
2023-01-26 11:46:11 +0100

    Legg til eksempel for mikrofrontends med astro

---
+++ astro/mikrofrontends/app-b/src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const App = () => (
+  <section>
+    <h2>App b</h2>
+  </section>
+);
+
+export default App;
---
+++ astro/mikrofrontends/shell/src/components/ReactComponent.jsx
@@ -0,0 +1,12 @@
+import React from "https://www.nav.no/tms-min-side-assets/react/18/esm/index.js"
+import MicroFrontendA from "http://localhost:7100/bundle.js"
+import MicroFrontendB from "http://localhost:7200/bundle.js"
+
+const MyReactComponent = () => (
+  <>
+    <MicroFrontendA />
+    <MicroFrontendB />
+  </>
+);
+
+export default MyReactComponent;

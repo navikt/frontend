9deb0a8640189179ca162b05e0618319ba23a93e
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2023-10-19 20:21:59 +0200

    Er kanskje best å flytte props opp

--- nextjs/dekoratoren/pages/_document.tsx
+++ nextjs/dekoratoren/pages/_document.tsx
@@ -6,25 +6,24 @@ import Document, {
   DocumentContext,
 } from "next/document";
 import {
-  DecoratorComponents,
-  DecoratorParams,
+  DecoratorComponents, DecoratorFetchProps,
   fetchDecoratorReact,
 } from "@navikt/nav-dekoratoren-moduler/ssr";
 import React from "react";
 
 const decoratorEnv: "dev" | "prod" = process.env.NAIS_CLUSTER_NAME === "prod-gcp" ? "prod" : "dev";
 
-const decoratorParams: DecoratorParams = {
-  context: "privatperson",
+const decoratorProps: DecoratorFetchProps = {
+  env: decoratorEnv,
+  params: {
+    context: "privatperson",
+  }
 };
 
 class _Document extends Document<{ decorator: DecoratorComponents }> {
   static async getInitialProps(ctx: DocumentContext) {
     const initialProps = await Document.getInitialProps(ctx);
-    const decorator = await fetchDecoratorReact({
-      env: decoratorEnv,
-      params: decoratorParams
-    });
+    const decorator = await fetchDecoratorReact(decoratorProps);
     return { ...initialProps, decorator };
   }
 

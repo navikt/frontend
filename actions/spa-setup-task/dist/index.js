5ca0a6c433142c979de805b7b6f30c3467568e9b
Thomas Krampl <85170275+thokra-nav@users.noreply.github.com>
2024-04-09 12:53:25 +0200

    Add missing `;` in configuration snippet of generated ingresses (#45)

--- actions/spa-setup-task/dist/index.js
+++ actions/spa-setup-task/dist/index.js
@@ -2704,7 +2704,7 @@ function ingressAnnotations(bucketPath, bucketVhost) {
         'nginx.ingress.kubernetes.io/backend-protocol': 'https',
         'nginx.ingress.kubernetes.io/server-snippet': `proxy_intercept_errors on;
 error_page 404 = /index.html;`,
-        'nginx.ingress.kubernetes.io/configuration-snippet': `more_set_headers "Cache-Control: public,max-age=0"
+        'nginx.ingress.kubernetes.io/configuration-snippet': `more_set_headers "Cache-Control: public,max-age=0";
 rewrite ^(.*)/$ ${bucketPath}/index.html break;
 rewrite ^/(.*)$ ${bucketPath}/$1 break;`
     };
